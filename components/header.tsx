bb8899e1a9eb6707c8285f841ffc025a91b184cb
Chris Griffing <cmgriffing@gmail.com>
2021-01-25 15:40:51 -0800

    fix: use logo and style appropriately

--- components/header.tsx
+++ components/header.tsx
@@ -10,18 +10,9 @@ export default function Header(props) {
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center max-w-screen-lg">
         <Link href="/">
           <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full"
-              viewBox="0 0 24 24"
-            >
-              <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-            </svg>
+            <div className="rounded-full bg-blue-500 p-2 w-12 h-12">
+              <img className="max-h-full w-auto" src="/logo-white.svg" />
+            </div>
             <span className="ml-3 text-xl">Vexilla</span>
           </a>
         </Link>
