d8867de57226c64b5d735244d019c4b91b185dcf
Chris Griffing <cmgriffing@gmail.com>
2021-02-19 15:58:13 -0800

    fix: change stacking order in home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -36,8 +36,8 @@ export default function HomePage() {
         </Nav>
       </Header>
       <Hero />
-      <HowItWorks />
       <WhatAreFeatureFlags />
+      <HowItWorks />
       <Features />
       <Documentation />
       <div>License</div>
